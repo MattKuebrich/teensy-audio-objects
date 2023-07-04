e493d6a91072a539b36a37165417b82744ec41dd
Matt Kuebrich <mattkuebrich@gmail.com>
2023-07-03 23:28:02 -0500

    add ringmodulator

---
+++ ringmodulator/effect_ringmodulator.cpp
@@ -0,0 +1,51 @@
+/* Audio Library for Teensy, Ring Modulator
+ * Copyright (c) 2023, Matt Kuebrich
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <Arduino.h>
+#include "effect_ringmodulator.h"
+
+void AudioEffectRingModulator::update(void)
+{
+  audio_block_t *blocka, *blockb;
+
+  blocka = receiveWritable(0);
+  blockb = receiveReadOnly(1);
+  if (!blocka) {
+    if (blockb) release(blockb);
+    return;
+  }
+  if (!blockb) {
+    release(blocka);
+    return;
+  }
+
+  for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+    int32_t a = blocka->data[i];
+    int32_t b = blockb->data[i];
+    int32_t ringmod = (a * b) >> 15; //same as divide by 32767, attenuates
+    blocka->data[i] = ringmod;
+  }
+
+  transmit(blocka);
+  release(blocka);
+  release(blockb);
+}
---
+++ ringmodulator/ringmodulator.ino
@@ -0,0 +1,54 @@
+/* Ring Modulator example
+
+This is just two sine waves being ring modulated.
+
+*/
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+#include "effect_ringmodulator.h"
+
+// GUItool: begin automatically generated code
+AudioSynthWaveformModulated carrier;            //xy=128.42859649658203,283.4285891056061
+AudioSynthWaveformModulated modulator;           //xy=598.428596496582,297.4285891056061
+AudioEffectRingModulator ringmodulator;         //xy=421.57152557373047,549.8571300506592
+AudioOutputUSB           usb1;           //xy=786.2381210327148,602.0951766967773
+AudioOutputI2S           i2s1;           //xy=968.4286117553711,308.42860221862793
+AudioConnection          patchCord7(modulator, 0, ringmodulator, 0);
+AudioConnection          patchCord8(carrier, 0, ringmodulator, 1);
+AudioConnection          patchCord9(ringmodulator, 0, i2s1, 0);
+AudioConnection          patchCord10(ringmodulator, 0, i2s1, 1);
+AudioConnection          patchCord11(ringmodulator, 0, usb1, 0);
+AudioConnection          patchCord12(carrier, 0, usb1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=981.7143249511719,255.00000858306885
+// GUItool: end automatically generated code
+
+#define POT01 14
+#define POT02 15
+
+void setup() {
+  AudioMemory(12);
+
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(.5);
+
+  carrier.begin(1.0, 100, WAVEFORM_SINE);
+  modulator.begin(1.0, 100, WAVEFORM_SINE);
+}
+
+void loop() {
+
+  carrier.frequency(mapf(analogRead(POT01), 0, 1023, 0.0, 1000));
+  modulator.frequency(mapf(analogRead(POT02), 0, 1023, 0.0, 1000));
+
+}
+
+
+// floating point map function taken from https://forum.arduino.cc/index.php?topic=361046.0
+double mapf(double x, double in_min, double in_max, double out_min, double out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
