2d509e92afbae1f87e2150d2882276a20cee157c
Matt Kuebrich <1854586+MattKuebrich@users.noreply.github.com>
2024-01-26 09:32:09 -0600

    Update chaosmaps.ino, remove fscale

--- chaosmaps/chaosmaps.ino
+++ chaosmaps/chaosmaps.ino
@@ -121,7 +121,6 @@ void loop() {
 
   // sample rate
   chaosmaps.sampleRate(mapf(pot03.getValue(), 0, 1023, 0.0, AUDIO_SAMPLE_RATE_EXACT)); 
-  //chaosmaps.sampleRate(fscale( 0.094, 1023.0, 0.0, 8000, pot03.getValue(), -6.5));
 
   // LFO 1, controls rate modulation
   mixer.gain(0, mapf(pot04.getValue(), 0, 1023, 0.0, 1.0)); // LFO amp
@@ -160,66 +159,4 @@ void memStats() {
   Serial.print("AUDIOMEMORY MAX = ");
   Serial.println(AudioMemoryUsageMax());
   Serial.println("");
-}
-
-//fscale for log response on frequency
-//https://playground.arduino.cc/Main/Fscale/
-float fscale( float originalMin, float originalMax, float newBegin, float
-              newEnd, float inputValue, float curve) {
-
-  float OriginalRange = 0;
-  float NewRange = 0;
-  float zeroRefCurVal = 0;
-  float normalizedCurVal = 0;
-  float rangedValue = 0;
-  boolean invFlag = 0;
-
-  // condition curve parameter
-  // limit range
-
-  if (curve > 10) curve = 10;
-  if (curve < -10) curve = -10;
-
-  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output
-  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
-
-  // Check for out of range inputValues
-  if (inputValue < originalMin) {
-    inputValue = originalMin;
-  }
-  if (inputValue > originalMax) {
-    inputValue = originalMax;
-  }
-
-  // Zero Refference the values
-  OriginalRange = originalMax - originalMin;
-
-  if (newEnd > newBegin) {
-    NewRange = newEnd - newBegin;
-  }
-  else
-  {
-    NewRange = newBegin - newEnd;
-    invFlag = 1;
-  }
-
-  zeroRefCurVal = inputValue - originalMin;
-  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
-
-
-  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine
-  if (originalMin > originalMax ) {
-    return 0;
-  }
-
-  if (invFlag == 0) {
-    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
-
-  }
-  else     // invert the ranges
-  {
-    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange);
-  }
-
-  return rangedValue;
 }
